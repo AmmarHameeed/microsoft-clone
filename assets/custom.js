8d43bbbaa9c0a555f6915292a21ef500cc6b4dd9
Ammar Hameed <ammarro444@gmail.com>
2021-10-31 14:03:15 +0500

    Adjusted dropdowns
    Adjusted dropdowns

--- assets/custom.js
+++ assets/custom.js
@@ -8,48 +8,64 @@ $(document).ready(function(){
     $(".modal-close,.modal-background").on("click", function(){
         $(".mobile-menu").hide();
     })
-    $(".theme-dropdown-trigger").on("click", function(){
-        $("#theme-menu").show();
-    })
+    
     $(".theme-selector.light-theme-custom").on("click", function(){
         $("html").addClass("theme-light");
         $("html").removeClass("theme-dark");
         $("html").removeClass("theme-high-contrast");
+        $("#theme-menu").hide();
     })
     $(".theme-selector.dark-theme-custom").on("click", function(){
         $("html").addClass("theme-dark");
         $("html").removeClass("theme-light");
         $("html").removeClass("theme-high-contrast");
+        $("#theme-menu").hide();
     })
     $(".theme-selector.highC-theme-custom").on("click", function(){
         $("html").addClass("theme-high-contrast");
         $("html").removeClass("theme-dark");
         $("html").removeClass("theme-light");
+        $("#theme-menu").hide();
     })
-    $(".dropdown-trigger").on("mouseover", function(){
-        console.log("clicked")
-        $(".dropdown-menu").hide();
-        $(this).siblings().css("display","block");
+    // $(".dropdown-trigger").on("click", function(){
+    //     console.log("clicked")
+    //     $(".dropdown-menu").hide();
+    //     $(this).siblings().css("display","block");
         
-    })
-    $(".dropdown-trigger").on("mouseout", function(){
-        console.log("clicked")
-        $(".dropdown-menu").hide();
-        // $(this).siblings().css("display","block");
+    // })
+    // $(".dropdown-trigger").on("mouseout", function(){
+    //     console.log("clicked")
+    //     $(".dropdown-menu").hide();
+    //     // $(this).siblings().css("display","block");
         
-    })
+    // })
 
     // $("body").on("click", function(e){
-    //     if($("#theme-menu").hasClass("dropdown-closed")){
-    //         e.preventDefault();
-    //         $("#theme-menu").removeClass("dropdown-closed")
-    //         $("#theme-menu").addClass("dropdown-open")
-    //         // $("#theme-menu").show();
-    //     }
-    //     else if(($("#theme-menu").hasClass("dropdown-open"))){
-    //         $("#theme-menu").removeClass("dropdown-open")
-    //         $("#theme-menu").addClass("dropdown-closed")
-    //         $("#theme-menu").hide();
-    //     }
+    //     var color = $('.mainmenu-dd').css('display');
+    //     // if($("#theme-menu").hasClass("dropdown-closed")){
+    //     //     e.preventDefault();
+    //     //     $("#theme-menu").removeClass("dropdown-closed")
+    //     //     $("#theme-menu").addClass("dropdown-open")
+    //     //     // $("#theme-menu").show();
+    //     // }
+    //     // else if(($("#theme-menu").hasClass("dropdown-open"))){
+    //     //     console.log("body click")
+    //     //     $("#theme-menu").removeClass("dropdown-open")
+    //     //     $("#theme-menu").addClass("dropdown-closed")
+    //     //     $("#theme-menu").hide();
+    //     // }
+    //     // if((color == "block")){
+    //     //     console.log("dropdown 2 click click")
+    //     //     $('.mainmenu-dd').hide();
+    //     // }
+    //     // else if((color == "none")){
+    //     //     $('.mainmenu-dd').show();
+    //     // }
     // })
+    $(".theme-dropdown-trigger").on("click", function(){
+        console.log("dropdown click")
+        // $("#theme-menu").removeClass("dropdown-closed")
+        //     $("#theme-menu").addClass("dropdown-open")
+        $("#theme-menu").show();
+    })
 })
